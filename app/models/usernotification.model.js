c8edc150133dd95a6ab685984acea2c5b68cf419
taruneeth <srikrish7.1991@gmail.com>
2024-02-08 22:33:32 +0530

    Notification settings logic updated

--- app/models/usernotification.model.js
+++ app/models/usernotification.model.js
@@ -2,6 +2,9 @@ const usernotificationModel = (sequelize, Sequelize) => {
      const usernotification = sequelize.define('usernotification', {
          id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
          option: { type: Sequelize.STRING, allowNull: true },
+         hours: { type: Sequelize.STRING, allowNull: true },
+         weeks: { type: Sequelize.STRING, allowNull: true },
+         days: { type: Sequelize.STRING, allowNull: true },
          enable: { type: Sequelize.INTEGER, allowNull: true },
          status: { type: Sequelize.INTEGER, defaultValue: 1 } //1- Success, 2 - failure, 3-In Progress
      })
